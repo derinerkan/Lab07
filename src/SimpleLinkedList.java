5039c92b56940b25c6cad0a23297a941177b994f
derin <derin24@gmail.com>
2017-04-28 20:31:19 +0300

    Removed the simplelistlink as it seems to be possible to implement code without it

--- src/SimpleLinkedList.java
+++ src/SimpleLinkedList.java
@@ -1,9 +1,25 @@
 /**
- * Actual SimpleLinkedList for Lab07
+ * SimpleLinkedList for Lab07
  * @author Derin Erkan
  * @version 2017-04-28
  */
 public class SimpleLinkedList
 {
+    private SimpleLinkedList next;
+    private SimpleLinkedList prev;
+    private String toStore;
+    /**
+     * Constructs a new SimpleLinkedList
+     */
+    public SimpleLinkedList();
 
+    public void addToHead( String item);
+
+    public String removeFromHead();
+
+    public boolean isEmpty();
+
+    public String get( int index);
+
+    public String toString();
 }
